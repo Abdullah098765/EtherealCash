6e3bc983ccc6669fe1707c17bcba1df1d2add186
AbdullahSabir098765 <ssmmhazz@gmail.com>
2025-01-08 17:22:51 +0500

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -7,9 +7,9 @@ export default function Document() {
         <meta charset="utf-8" />
         <meta http-equiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/img/favicon.png" />
-        <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.png" />
-        <link rel="apple-touch-icon" href="/img/favicon.png" />
+        <link rel="icon" href="/img/favicon-3.png" />
+        <link rel="shortcut icon" type="image/x-icon" href="/img/favicon-3.png" />
+        <link rel="apple-touch-icon" href="/img/favicon-3.png" />
       </Head>
 
       <body>
