8b4f294ccf45852c014be503229b70e6df6c8358
AbdullahSabir098765 <ssmmhazz@gmail.com>
2025-01-14 14:29:42 +0500

    blogSEO!@

--- pages/_document.js
+++ pages/_document.js
@@ -8,13 +8,18 @@ export default function Document() {
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-8EXD2R1PK1"
         ></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'G-8EXD2R1PK1');
-        </script>
-        <meta charset="utf-8" />
-        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8EXD2R1PK1');
+            `,
+          }}
+        />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/img/favicon-3.png" />
         <link
