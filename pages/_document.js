dcb5aad68ad5b4c09bee9f18d9bf425fa9727227
AbdullahSabir098765 <ssmmhazz@gmail.com>
2025-01-14 14:20:33 +0500

    blogSEO

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,24 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-8EXD2R1PK1"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-8EXD2R1PK1');
+        </script>
         <meta charset="utf-8" />
         <meta http-equiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/img/favicon-3.png" />
-        <link rel="shortcut icon" type="image/x-icon" href="/img/favicon-3.png" />
+        <link
+          rel="shortcut icon"
+          type="image/x-icon"
+          href="/img/favicon-3.png"
+        />
         <link rel="apple-touch-icon" href="/img/favicon-3.png" />
       </Head>
 
